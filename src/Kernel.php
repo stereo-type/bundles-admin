c40ea366e1ee45a481bf6bd96635b7810ecbe355
Жалялетдинов Вячеслав <evil_tut@mail.ru>
2025-07-08 18:03:38 +0300

    ARH-1019 configs

---
+++ src/Kernel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace AcademCity\AdminBundle;
+
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+
+class Kernel extends BaseKernel
+{
+    use MicroKernelTrait;
+}
